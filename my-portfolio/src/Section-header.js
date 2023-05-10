e30da9dcf1d42dfbd551cb201060d1d68abbe4ab
Daniel Rasch <97550201+danielr0007@users.noreply.github.com>
2023-05-09 21:08:37 -0400

    Delete my-portfolio directory

--- my-portfolio/src/Section-header.js
+++
@@ -1,9 +0,0 @@
-function SectionHeader({ title }) {
-  return (
-    <div className="title-container light-border-right">
-      <h2>{title}</h2>
-    </div>
-  );
-}
-
-export default SectionHeader;
